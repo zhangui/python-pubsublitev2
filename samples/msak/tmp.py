9e3318c0cc8933eacfa14dfa939f59cc77c33941
Yang Zhang <yang10.zhang@gmail.com>
2025-09-30 21:29:16 -0400

    conusmer update

--- samples/msak/tmp.py
+++ samples/msak/tmp.py
@@ -9,13 +9,3 @@ client = pubsublite_v1.PublisherServiceClient(
     producer_config=producer_config,
 )
 print('SUCCESS: Client created')
-
-# 1. Clear Python bytecode cache for publisher_service
-  find google/cloud/pubsublite_v1/services/publisher_service -name "*.pyc" -delete
-  find google/cloud/pubsublite_v1/services/publisher_service -name "__pycache__" -type d -exec rm -rf {} + 2>/dev/null
-
-  # 2. Clear all Python bytecode cache in the entire project (more thorough)
-  find /Users/yangzhang/Desktop/psl/python-pubsublite -name "*.pyc" -delete
-  find /Users/yangzhang/Desktop/psl/python-pubsublite -name "__pycache__" -type d -exec rm -rf {} + 2>/dev/null
-
-find . -type f -name "*.pyc" -delete && find . -type d -name "__pycache__" -exec rm -rf {} + 2>/dev/null
