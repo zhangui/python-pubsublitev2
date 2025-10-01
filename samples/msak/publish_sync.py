92684daf99056af95df1b892dbdf7effde70227a
Yang Zhang <yang10.zhang@gmail.com>
2025-09-30 22:44:59 -0400

    fix producer

--- samples/msak/publish_sync.py
+++ samples/msak/publish_sync.py
@@ -90,7 +90,7 @@ def publish_messages_kafka(
     stream = client.publish(requests=request_generator())
 
     for i, response in enumerate(stream):
-        print(f"Published message {i}: offset=fff")
+        print(f"Published message {i}: offset={response.message_response}")
 
 
 if __name__ == "__main__":
